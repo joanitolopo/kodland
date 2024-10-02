d24a0152678688875fe567fa797b305f120fc029
joan <68073738+joanitolopo@users.noreply.github.com>
2024-10-02 20:24:30 +0700

    Update connect.py
    ubah kelas klien ke kelas bot

--- python-pro-m1/connect.py
+++ python-pro-m1/connect.py
@@ -1,34 +1,31 @@
 import discord
+from discord.ext import commands
 from bot_logic import gen_pass
-from bot_logic import gen_emodji
 
-with open("token.txt", "r") as f: # Membaca token dari file token.txt
-    token = f.read() # Menyimpan token ke dalam variabel token
-
-# Variabel intents menyimpan hak istimewa bot
 intents = discord.Intents.default()
-# Mengaktifkan hak istimewa message-reading
 intents.message_content = True
-# Membuat bot di variabel klien dan mentransfernya hak istimewa
-client = discord.Client(intents=intents)
 
-@client.event
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
 async def on_ready():
-    print(f'Kita telah masuk sebagai {client.user}')
+    print(f'We have logged in as {bot.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith('$halo'):
-        await message.channel.send("Hi!")
-    elif message.content.startswith('$bye'):
-        await message.channel.send("\\U0001f642")
-    elif message.content.startswith('$buat kata sandi'):
-        await message.channel.send(gen_pass(5))
-    elif message.content.startswith("$buat emotikon"):
-        await message.channel.send(gen_emodji())
-    else:
-        await message.channel.send(message.content)
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hai! Saya adalah bot {bot.user}!') # $hello
 
-client.run(token)
+@bot.command()
+async def pasw(ctx):
+    await ctx.send(gen_pass(10)) # $pasw
+
+@bot.command()
+async def zakkir(ctx):
+    await ctx.send("Hi! nama saya zakkir!!!") # $zakkir
+
+@bot.command()
+async def heh(ctx, count_number = 10, nabila=10):
+    await ctx.send("he" * count_number + nabila) # $heh 100 60 
+
+
+bot.run("")
